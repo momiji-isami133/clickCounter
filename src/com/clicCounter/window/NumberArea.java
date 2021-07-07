e561e6d8406f0e888b5f1bc8f83ce57fc2ada844
momiji-isami133 <momiji.isami133@gmail.com>
2021-07-08 00:16:31 +0900

    [Add] JavaDoc - API_Reference

--- src/com/clicCounter/window/NumberArea.java
+++ src/com/clicCounter/window/NumberArea.java
@@ -2,16 +2,33 @@ package com.clicCounter.window;
 
 import javax.swing.JLabel;
 
+/**
+ * 
+ * @author momiji-isami133
+ *
+ *整数型の値を格納するラベルクラス
+ */
 public class NumberArea extends JLabel {
 	
+	/**
+	 * コンストラクタ
+	 */
 	public NumberArea() {
 		super();
 	}
 	
+	/**
+	 * コンストラクタ
+	 * @param n 整数型の値
+	 */
 	public NumberArea(Integer n) {
 		super(n.toString());
 	}
 	
+	/**
+	 * 整数型の値をセット
+	 * @param n 整数型の値
+	 */
 	public void setNumber(Integer n) {
 		super.setText(n.toString());
 	}
--- src/com/clicCounter/window/Window.java
+++ src/com/clicCounter/window/Window.java
@@ -13,13 +13,26 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+/**
+ * 
+ * @author momiji-isami133
+ * 
+ * ウィンドウを作成するクラス
+ */
 public class Window extends JFrame implements ActionListener{
-	private NumberArea numberArea;
+	/** 増減ボタン */
 	private ArrayList<JButton> buttons;
+	/** カウント */
 	private Integer count;
+	/** 画面 */
 	private Container contentPane;
+	/** ボタン配置用パネル */
 	private JPanel buttonPane;
+	/** カウント表示領域 */
+	private NumberArea numberArea;
+	/** テキスト表示領域 */
 	private JLabel textArea;
+	/** フォント */
 	private Font font;;
 	
 	/** 引数なしコンストラクタ */
@@ -32,20 +45,32 @@ public class Window extends JFrame implements ActionListener{
 	 * @param title タイトル
 	 */
 	public Window(int width,int height,String title) {
+		count = 0;
+		// ウィンドウの初期化
 		initializeFrame(width, height, title);
 		
+		// ボタンを一つずつ登録
 		for(JButton button:buttons) {
 			button.addActionListener(this);
 			buttonPane.add(button);
 		}
+		// 画面上部にテキストを表示
 		contentPane.add(textArea, BorderLayout.PAGE_START);
+		// 画面中央にカウントを表示
 		contentPane.add(numberArea, BorderLayout.CENTER);
+		// 画面下部にボタンを表示
 		contentPane.add(buttonPane, BorderLayout.PAGE_END);
 		
 	}
 	
+	/**
+	 * ボタン押下時の制御
+	 * @param e ボタン押下時のイベント
+	 */
 	public void actionPerformed(ActionEvent e) {
+		// 押されたボタンを取得
 		String cmd = e.getActionCommand();
+		// ボタンに応じて処理を切り替え
 		switch(cmd) {
 		case "-10":
 			count -= 10;
@@ -79,18 +104,27 @@ public class Window extends JFrame implements ActionListener{
 		}
 	}
 	
+	/**
+	 * ウィンドウ作成時の初期化
+	 * @param width 画面の横幅
+	 * @param height 画面の立幅
+	 * @param title 画面名
+	 */
 	public void initializeFrame(int width,int height,String title) {
-//		getContentPane().setLayout(new FlowLayout());
-		
+		// 画面名の設定
 		setTitle(title);
+		// 画面サイズの設定
 		setSize(width, height);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// 画面のリサイズの可否設定
 		setResizable(false);
 		
+		// フォント設定
 		font = new Font("Times new roman", Font.PLAIN, 24);
 		
+		// ボタン初期化
 		buttons = initializeButton();
-		count = 0;
+		
 		numberArea = new NumberArea(count);
 		numberArea.setFont(font);
 		numberArea.setHorizontalAlignment(JLabel.CENTER);
@@ -101,6 +135,10 @@ public class Window extends JFrame implements ActionListener{
 		buttonPane = new JPanel();
 	}
 	
+	/**
+	 * ボタンを初期化する
+	 * @return 初期化されたボタンのリスト
+	 */
 	public ArrayList<JButton> initializeButton() {
 		JButton[] buttnList = {
 			new JButton("-10"),
